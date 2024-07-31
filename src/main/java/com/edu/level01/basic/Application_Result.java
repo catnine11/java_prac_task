9587cda1e51e923127c6f9fc010f50a343dec64b
catnine11 <catnine11@gmail.com>
2024-07-31 17:56:03 +0900

    1chap 완료, 3chap의 1까지 완료

---
+++ src/main/java/com/edu/level01/basic/Application_Result.java
@@ -0,0 +1,41 @@
+package com.edu.level01.basic;
+
+import java.util.Scanner;
+
+public class Application_Result {
+    public static void main(String[] args) {
+        /*모든 메소드는 main 함수에서 호출하여 출력시킨다.*/
+        Calculator cal = new Calculator();
+
+        /*메소드 호출 확인
+        1부터 10까지의 합 : 55
+        두 수 중 큰 수는 20이다.
+        10과 20의 합은 : 30
+        10과 5의 차는 : */
+        cal.checkMethod();
+        cal.sum1to10();
+
+        System.out.println("비교할 수 두 개를 입력해주세요");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        cal.checkMaxNumber(a, b);
+
+        System.out.println("합 구하기");
+        int c = sc.nextInt();
+        System.out.print("과 ");
+        int d = sc.nextInt();
+        System.out.print("의 합은 : ");
+        System.out.println(cal.sumTwoNumber(c, d));
+
+        System.out.println("차 구하기");
+        int e = sc.nextInt();
+        System.out.println("과 ");
+        int f = sc.nextInt();
+        System.out.print("의 차는 : ");
+        System.out.println(cal.minusTwoNumber(e,f));
+
+
+
+    }
+}
---
+++ src/main/java/com/edu/level01/basic/Calculator.java
@@ -0,0 +1,38 @@
+package com.edu.level01.basic;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public void checkMethod(){
+        /*함수 호출 확인용 메소드*/
+        System.out.println("메소드 호출 확인");
+    }
+
+    public int sum1to10(){
+        /*1부터 10까지 수를 더하여 값을 리턴함*/
+        int sum = 1+2+3+4+5+6+7+8+9+10;
+        return sum;
+    }
+
+    public void checkMaxNumber(int a, int b){
+        /*두 수를 입력받아 큰 수를 출력함*/
+
+        System.out.println("두 수 중 큰 수는 " + Math.max(a, b) + "이다.");
+    }
+
+    public int sumTwoNumber(int a, int b){
+        /*두 수를 입력받아 합을 리턴함*/
+//        int result = a + b;
+//        System.out.println(a + "과 " + b + "의 합은 : " + result);
+        return a + b;
+    }
+
+    public int minusTwoNumber(int a, int b){
+        /*두 수를 입력받아 차를 리턴함*/
+
+        return a - b;
+    }
+
+
+}
